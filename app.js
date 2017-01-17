c82e95f1ed2d1bc7c29a5aaaec5dc0c868d20339
Aria Moraine <ariamoraine@gmail.com>
2017-01-17 17:46:57 -0500

    adding some comments

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 const express = require('express');
-const nunjucks = require("nunjucks");
-const bodyParser = require("body-parser");
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
 const app = express();
-
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', {
-	noCache: true
-}); // point nunjucks to the proper directory for templates
-
 const routes = require('./routes/');
 
-app.listen(3000, function() {
-	console.log("Server listening");
-});
-
+//static routing
+app.use('/', routes);
 app.use(express.static('public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
+//setting up nunjucks
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+app.set('view engine', 'html'); // have res.render work with html files
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
 
-// parse application/json
-app.use(bodyParser.json())
+///////////////////////////
+// next two lines are for parsing POST body submission MIDDLEWARE
+//////////////////////////
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
 
-app.use('/', function(req, res, next) {
-	console.log("Request:", req.method, req.path);
 
-	next();
+app.listen(3000, function() {
+  console.log('Server listening');
 });
 
-app.use('/', routes);
-
-app.use('/special', function(req, res, next) {
-	console.log("You have reached the special page.");
-	next();
+app.use('/', function(req, res, next) {
+  console.log('Request:', req.method, req.path);
+  next();
 });
--- routes/index.js
+++ routes/index.js
@@ -11,33 +11,19 @@ router.post('/tweets', function(req, res) {
 
 router.get('/', function(req, res) {
 	let tweets = tweetBank.list();
-	res.render('index', {
-		tweets: tweets,
-		showForm: true
-	});
+	res.render('index', { tweets: tweets, showForm: true });
 });
-
 
 router.get('/users/:name', function(req, res) {
 	var name = req.params.name;
-	var list = tweetBank.find({
-		name: name
-	});
-	res.render('index', {
-		tweets: list
-	});
+	var list = tweetBank.find({ name: name });
+	res.render('index', { tweets: list });
 });
-
 
 router.get('/tweets/:id', function(req, res) {
 	var id = Number(req.params.id);
-	var list = tweetBank.find({
-		id: id
-	});
-	res.render('index', {
-		tweets: list
-	});
+	var list = tweetBank.find({ id: id });
+	res.render('index', { tweets: list });
 });
-
 
 module.exports = router;
