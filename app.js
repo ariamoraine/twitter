5ddddef7f69fc9df43d7a8c38fafc365a8825d3f
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-01-17 12:58:56 -0500

    working on adding a single-user route

--- app.js
+++ app.js
@@ -8,17 +8,6 @@ nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper 
 
 const routes = require('./routes/');
 
-// var locals = {
-//   title: 'An Example',
-//   people: [
-//     { name: 'Gandalf'},
-//     { name: 'Frodo' },
-//     { name: 'Hermione'}
-//   ]
-// };
-
-// const dogs = [{name: 'fido'}, {name: 'Stacker'}, {name: 'Son'}];
-
 app.listen(3000, function(){
   console.log("Server listening");
   });
@@ -27,7 +16,7 @@ app.use(express.static('public'));
 
 app.use('/', function (req, res, next) {
  console.log("Request:", req.method, req.path);
-  //res.sendFile('../public/stylesheets/style.css', {root : __dirname});
+
  next();
 });
 
@@ -38,19 +27,4 @@ app.use('/special', function (req, res, next) {
  next();
 });
 
-// app.get('/', function(req, res, next){
-//   //res.send("Welcome!");
-//   nunjucks.configure('views', {noCache: true});
-//   nunjucks.render('index.html', locals, function (err) {
-//     res.render('index', {title: 'Hall of Fame', people: dogs});
-//   });
-// });
 
-// app.get('/news', function(req, res, next){
-//   res.send("Welcome to the news page!");
-// });
-
-// app.use(function (err, req, res, next) {
-//   console.error("ERROR!", err.msg || "idk!");
-//   res.sendStatus(err.status || 400);
-// });
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,23 @@ router.get('/', function (req, res) {
   res.render( 'index', { tweets: tweets } );
 });
 
+
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  console.log(name);
+  var list = tweetBank.find( {name: name} );
+  console.log(list); 
+  res.render( 'index', {list: list} );
+});
+
 module.exports = router;
+
+
+// router.get('/users/:name', function(req, res) {
+//   var name = req.params.name;
+//   console.log(name); 
+//   var list = tweetBank.find(function(obj){return obj.name.split(" ").join("") === name});
+//   console.log(list); 
+//   res.render( 'index', list.forEach(function(obj){return obj.content}));
+// });
